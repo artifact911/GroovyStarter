ade17ebba2a078f3156261ac5fa02cf8e005aa85
artifact911 <artifact911@gmail.com>
2022-09-25 18:45:34 +0300

    lesson4 classLoader done

--- src/com/artifact/HelloWorld.groovy
+++ src/com/artifact/HelloWorld.groovy
@@ -2,6 +2,8 @@ package com.artifact
 
 import groovy.transform.PackageScope
 
+import java.sql.DriverManager
+
 class HelloWorld {
 
     // ; - не нежна
@@ -9,7 +11,21 @@ class HelloWorld {
 
 //    @PackageScope
     static void main(String[] args) {
+
         // () и ; - не нужны
         println "Hello World!"
+
+        // Зачем нужны ClassLoader? Они и занимаются загрузкой объектов наших классов в MetaSpace соответственно каждый
+        // объект класса Class знает о ClassLoader который его загрузил
+        String value = "Hello World"
+        Class<String> clazz = value.getClass()
+        assert clazz == String.class
+
+        // вернет null. - потому что это Bootstrap ClassLoader
+        println clazz.getClassLoader()
+        // а тут вернет PlatformClassLoader
+        println DriverManager.class.getClassLoader()
+        // вернет AppClassLoader
+        println HelloWorld.class.getClassLoader()
     }
 }
